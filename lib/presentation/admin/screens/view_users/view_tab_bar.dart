143e0cdef56d606159a30603a49b110cb4f031bd
Shibil-Basith <shibilbasith4u@gmail.com>
2023-04-11 22:51:59 +0530

    First commit

---
+++ lib/presentation/admin/screens/view_users/view_tab_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:salesafari/core/colors/Colors.dart';
+import 'package:salesafari/presentation/admin/screens/view_users/view_customers.dart';
+import 'package:salesafari/presentation/admin/screens/view_users/view_sellers.dart';
+
+class ViewTabBar extends StatelessWidget {
+  const ViewTabBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            title: const Text(
+              'Admin Dashboard',
+              style: TextStyle(
+                fontSize: 27,
+              ),
+            ),
+            actions: [
+              IconButton(
+                iconSize: 35,
+                onPressed: () {
+                  Navigator.pushNamed(context, 'adminprofile');
+                },
+                icon: const Icon(
+                  Icons.account_circle_outlined,
+                ),
+              ),
+            ],
+            // ignore: prefer_const_constructors
+            bottom:TabBar(tabs: 
+             const [
+              Tab(child: Text('View Sellers',style: TextStyle(color: textColor),),),
+              Tab(child: Text('View Customers',style: TextStyle(color: textColor),),),
+            ]
+            ),
+          ),
+          body:const TabBarView(
+            children: [
+              ViewSellersPage(),
+              ViewCustPage()
+            ]
+            ),
+      ),
+    );
+  }
+}
